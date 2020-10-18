7ce1e0dbad5df4da60170d0211632884bac6fb84
Thiti Dev <thiti.mwk.main@gmail.com>
2020-10-18 23:27:54 +0700

    :sparkles: The express server is settle

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,14 @@
-console.log('Nothings much yet')
+import express from 'express'
+import { HANDLE_unhandled_promise_rejections } from './handlers/server';
+import { INIT_Middlewares } from './middlewares';
+import { INIT_Routes } from './routes';
+const app = express()
+INIT_Middlewares(app) // init all middlewares
+INIT_Routes(app) // init all routes
+const PORT = process.env.PORT || 5000;
+const server = app.listen(PORT, () => {
+    console.log(
+        `The server is currently running on port ${PORT}`
+    );
+});
+HANDLE_unhandled_promise_rejections(server)
